ee35509be7ab9e42c5a41a0dab750f23ea3e1e2d
arunipatel06 <aruniptel06@gmail.com>
2020-09-30 11:16:57 -0400

    create schema for user and receipe

--- schema.js
+++ schema.js
@@ -1,2 +1,20 @@
-exports.typeDefs = `
+const { gql } = require("apollo-server");
+exports.typeDefs = gql`
+  type Recipe {
+    name: String!
+    category: String!
+    description: String!
+    createdDate: String
+    instructions: String!
+    likes: Int
+    username: String!
+  }
+
+  type User {
+    username: String! @unique
+    password: String!
+    joinDate: String
+    email: String
+    favourites: [Recipe]
+  }
 `;
