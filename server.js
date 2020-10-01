87b0405fbebd627813a924e61c261572399fe3a0
arunipatel06 <aruniptel06@gmail.com>
2020-10-01 10:44:33 -0400

    write query and mutation

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.get("/", async function (req, res) {
   const db = await mongo();
+  console.log("DB Connected.....");
 
   db.collection("userlist")
     .find()
@@ -25,43 +26,3 @@ const server = new ApolloServer({ typeDefs, resolver });
 server.listen(PORT, () => {
   console.log(`Server is ready on http://localhost:${PORT} `);
 });
-
-// const mongoose = require("mongoose");
-// const Recipe = require("./models/Recipe");
-// const User = require("./models/User");
-// const uri = process.env.MONGODB_URL;
-
-// //bring graphql-express middleware
-// const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
-// const { makeExecutableSchema } = require("graphql-tools");
-
-// const { typeDefs } = require("./schema");
-// const resolver = require("./resolver");
-
-// //create schema
-// const schema = makeExecutableSchema({
-//   typeDefs,
-//   resolver,
-// });
-
-// //connect with database
-// mongoose
-//   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log("DB CONNECTED"))
-//   .catch((err) => console.log("Error: ", err.message));
-
-// const app = express();
-
-// app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
-
-// //connect schema with GraphQl
-// app.use(
-//   "/graphql",
-//   graphqlExpress({
-//     schema,
-//     context: {
-//       User,
-//       Recipe,
-//     },
-//   })
-// );
