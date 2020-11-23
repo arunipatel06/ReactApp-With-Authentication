fb937c14cec397b05cac0f9af6090e4043ba9df8
arunipatel06 <arunipatel06@gmail.com>
2020-11-23 13:47:57 -0500

    update apollo client logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,34 @@
-import React from "react";
-import "./App.css";
-import { Query } from "react-apollo";
-import { GETALLRECIPES } from "../queries";
+import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+// import ExchangeRates from "./ExchangeRates";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const App = () => (
-  <div className="App">
-    <h1>Home Page</h1>
-    <Query query={GETALLRECIPES}>
-      {({ data, loading, error }) => {
-        if (loading) return <div>Loading</div>;
-        if (error) return <div>Error</div>;
-        console.log(data);
-        return <p>Recipes</p>;
-      }}
-    </Query>
-  </div>
-);
+import './App.css';
+import UserList from './Form_Practice/UserList';
+import VehicleData from './VehicleData';
+import Practice from './Practice';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <ApolloProvider client={client}>
+            <Route exact path="/userlist" component={UserList} />
+            {/* <Route path="/vehicle/:id" component={ExchangeRates} /> */}
+            <Route path="/data/:id" component={VehicleData} />
+            <Route path="/info/:id" component={Practice} />
+          </ApolloProvider>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
