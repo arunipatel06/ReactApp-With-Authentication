363a5d3b7fcc80844b966f14aa2fe89df228fb6f
arunipatel06 <arunipatel06@gmail.com>
2020-11-29 17:57:06 -0500

    add JSS property of components

---
+++ client/src/JssStyleSheets/JssLogin.js
@@ -0,0 +1,95 @@
+import { makeStyles } from '@material-ui/core/styles';
+import LoginForm from '../assests/LoginForm.jpg';
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    background: '#FAFAFA',
+  },
+  body: {
+    position: 'relative',
+    width: '365px',
+    backgroundColor: theme.palette.background.default,
+    margin: '100px auto 0px',
+    outline: 0,
+    overflowY: 'hidden',
+    [theme.breakpoints.up('md')]: {
+      width: '900px',
+    },
+  },
+  formContainer: {
+    overflow: 'hidden',
+    padding: '90px 50px 140px',
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '20px',
+  },
+  title: {
+    fontFamily: '"Mada", sans-serif',
+    fontWeight: 600,
+    fontSize: '36px',
+    lineHeight: '51px',
+    color: '#1b242f',
+  },
+  left: {
+    textAlign: 'left',
+  },
+  textTitle: {
+    fontFamily: '"Mada", sans-serif',
+    fontWeight: '600',
+    fontSize: '18px',
+    lineHeight: '28px',
+    margin: '0px 0px 10px 0px',
+    color: '#1b242f',
+  },
+  TextField: {
+    fontFamily: 'sans-serif',
+    width: '100%',
+  },
+  button: {
+    fontWeight: '600',
+    color: 'white',
+    background: '#c5b1db',
+    padding: '10px 27px',
+    textTransform: 'none',
+    width: '250px',
+  },
+  signUpButton: {
+    backgroundColor: 'transparent',
+  },
+  signUpText: {
+    textAlign: 'center',
+    padding: '10px 55px',
+  },
+  LoginImage: {
+    width: '50%',
+    textAlign: 'center',
+    backgroundImage: `url(${LoginForm})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    padding: '0px',
+  },
+  imageTitle: {
+    margin: '30',
+    fontFamily: '"Mada", sans-serif',
+    fontWeight: 600,
+    fontSize: '36px',
+    lineHeight: '51px',
+    color: '#ffffff',
+  },
+  imageText: {
+    fontFamily: '"Mada", sans-serif',
+    fontWeight: 400,
+    fontSize: '20px',
+    lineHeight: '40px',
+    color: '#ffffff',
+  },
+}));
---
+++ client/src/JssStyleSheets/JssNavbar.js
@@ -0,0 +1,55 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme) => ({
+  navbar: {
+    width: '100%',
+    height: '50px',
+    position: 'fixed',
+    left: 0,
+    top: 0,
+    // bottom: 0,
+    right: 0,
+    bottom: 'auto',
+    textAlign: 'left',
+    transition: '0.25s',
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    zIndex: 1000,
+    backgroundColor: 'transparent',
+    // borderBottom: '2px solid #b9a61c',
+    // boxShadow: '4px 4px 10px 0 rgba(0, 0, 0, 0.3)',
+  },
+  button: {
+    transition: '0.5s',
+    fontFamily: '"Mada", sans-serif',
+    color: '#723b3b',
+    // color: '#18465b',
+    fontSize: '25px',
+    fontWeight: 'bold',
+    margin: '0px 20px',
+    '&:hover': {
+      // backgroundColor: 'transparent',
+    },
+  },
+  buttonContainer: {
+    height: '50px',
+    float: 'left',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: '25px',
+  },
+  wContainer: {
+    height: '100%',
+    // margin: '0 auto',
+    display: 'flex',
+    justifyContent: 'right',
+    alignItems: 'right',
+    '&:after': {
+      content: '',
+      display: 'block',
+      clear: 'both',
+    },
+  },
+}));
